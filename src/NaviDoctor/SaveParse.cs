81c14535d0fb75f1b92583a0fce5f55043649fbb
functionFox <thefunctionfox@gmail.com>
2023-07-19 02:10:02 -0700

    Added a fix for BN2 Hard Mode files that got nuked in the Build 11463040 update.

--- src/NaviDoctor/SaveParse.cs
+++ src/NaviDoctor/SaveParse.cs
@@ -616,6 +616,10 @@ namespace NaviDoctor
                     break;
 
                 case GameTitle.Title.MegaManBattleNetwork2:
+                    if (saveFilePath.Contains("exe2j_hard_0.bin") && saveData[0xF8] != 0)
+                    { // Fix for BN2 Hard Mode files that got nuked in the 11463040 build update
+                        Buffer.BlockCopy(new byte[] { 0, 0, 0, 0 }, 0, saveData, 0xF8, 4);
+                    }
                     Buffer.BlockCopy(BitConverter.GetBytes(saveDataObject.CurrHP), 0, saveData, HPRedundancy1, 2);
                     Buffer.BlockCopy(BitConverter.GetBytes(saveDataObject.MaxHP), 0, saveData, HPRedundancy2, 2);
 
